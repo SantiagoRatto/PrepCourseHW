c0580631eea9436b86a68ba81347fc8f0056411a
Santiago Ratto <1santiago.r98@gmail.com>
2024-08-06 08:31:03 -0300

    modulo 5 completo

--- C5 Fundamentos Javascript/ejercicios/08.js
+++ C5 Fundamentos Javascript/ejercicios/08.js
@@ -7,6 +7,17 @@ function esNumeroEntero(numero) {
   // -1212 ---> true 
   // 121.212 ---> false 
   // Tu código:
+  if(typeof numero != "number"){
+    return (false);
+  } else if (numero % 1 == 0){
+    return (true);
+  } else {
+    return (false);
+  }
 }
+esNumeroEntero(0);
+console.log(0 % 1);
+
+
 
 module.exports = esNumeroEntero;
